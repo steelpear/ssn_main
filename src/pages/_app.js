6a63d01389387aa192dfd0295bd4839e411d5ccf
steelpear <steelpear@gmail.com>
2024-11-24 11:48:17 +0300

    start

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import "@/styles/globals.css";
+import "primereact/resources/themes/tailwind-light/theme.css"
+import { PrimeReactProvider } from 'primereact/api'
+import 'primereact/resources/primereact.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import '@/styles/main.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const value = {
+    ripple: true
+  }
+  
+  return (
+    <PrimeReactProvider value={value}>
+      <Component {...pageProps} />
+    </PrimeReactProvider>
+  )
 }
