2d393aff80a595a2779c4435932bcef02365f9a9
steelpear <steelpear@gmail.com>
2025-04-06 14:10:38 +0300

    fix blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,23 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import { useRouter } from 'next/navigation'
-import { Image } from 'primereact/image'
-import { Tag } from 'primereact/tag'
-import { Chip } from 'primereact/chip'
-import { Divider } from 'primereact/divider'
+import { OverlayPanel } from 'primereact/overlaypanel'
+import { 
+  MailruShareButton,
+  OKShareButton,
+  TelegramShareButton,
+  VKShareButton,
+  MailruIcon,
+  OKIcon,
+  TelegramIcon,
+  VKIcon
+} from 'react-share'
 
 export const Blog = () => {
+  const op = useRef(null)
   const router = useRouter()
+  const [shareUrl, setShareUrl] = useState(null)
+  const [shareTitle, setShareTitle] = useState(null)
+  const size = 30
   const [articles, setArticles] = useState(null)
   const [filter, setFilter] = useState({
     public: true,
@@ -26,9 +37,18 @@ export const Blog = () => {
     getArticles()
   },[filter])
 
+  const share = (e, article) => {
+    setShareUrl(`https://pro100tur.ru/article/${article.slug}`)
+    setShareTitle(article.title)
+    op.current.toggle(e)
+  }
+
   const itemTemplate = item => (<div className='col-4 max-h-16rem'>
     <img src={item.img} className='w-full h-full border-round-xl cursor-pointer' style={{objectFit: 'cover'}} onClick={() => router.push(`/article/${item.slug}`)} />
-    <div className='mx-1 text-sm text-600'>{new Date(item.date).toLocaleDateString()}</div>
+    <div className='flex align-items-center justify-content-between m-1'>
+      <div className='text-sm text-600'>{new Date(item.date).toLocaleDateString()}</div>
+      <i className='pi pi-share-alt cursor-pointer' style={{ fontSize: '1rem', color: '#708090' }} onClick={(e) => share(e, item)} />
+    </div>
     <div className='text-lg cursor-pointer line-height-2 hover:text-blue-700' onClick={() => router.push(`/article/${item.slug}`)}>{item.title}</div>
   </div>)
 
@@ -48,6 +68,26 @@ export const Blog = () => {
         <div className='grid'>
           {articles && articles.map(item => (itemTemplate(item)))}
         </div>
+        <OverlayPanel
+          ref={op}
+          pt={{
+            content: {className: 'pt-4 pb-2'}
+          }}>
+          <div className='grid gap-1'>
+            <MailruShareButton url={shareUrl} title={shareTitle}>
+              <MailruIcon round size={size} />
+            </MailruShareButton>
+            <OKShareButton url={shareUrl} title={shareTitle}>
+              <OKIcon round size={size} />
+            </OKShareButton>
+            <VKShareButton url={shareUrl} title={shareTitle}>
+              <VKIcon round size={size} />
+            </VKShareButton>
+            <TelegramShareButton url={shareUrl} title={shareTitle}>
+              <TelegramIcon round size={size} />
+            </TelegramShareButton>
+          </div>
+        </OverlayPanel>
       </main>
     )
   } else {return (<></>)}
